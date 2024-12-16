71247f85dbdad751cdcbea49a495073c79ce9084
Maral <martinovalexei568@mail.com>
2024-12-16 19:47:47 +0300

    gotovo

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework_assignments = 12
+Number_of_hours_spent = 1.5
+Course_title = "Python"
+Time_for_one_task = (Number_of_hours_spent / Number_of_completed_homework_assignments)
+print (Course_title, ", всего задач:", Number_of_completed_homework_assignments, ", затрачено часов:", Number_of_hours_spent, ", среднее время выполнения", Time_for_one_task, "часа")
