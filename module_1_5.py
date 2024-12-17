71e4d9576bf663ad12299e50a64465149c29c994
Maral <martinovalexei568@mail.com>
2024-12-17 12:12:35 +0300

    gotovo

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = ([3,5], 10, "РВСН", True)
+print(immutable_var)
+# immutable_var [2] = 4 # элементы кортежа нельзя изменить, кортеж не изменяемый
+mutable_list = [1,6,10]
+mutable_list [0] = 2
+print(mutable_list)
